d98c86640cf9ef9e9b4c68d0ac33bab6e4f09d8e
Caio Rocha <rochacaiodev@gmail.com>
2025-06-01 01:01:07 -0300

    feature: finish first exercise

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-import ScreenSaver from '../../../components/ScreenSaver';
+import Link from 'next/link';
 
-function ScreenSaverExercise() {
+function ScreenSaverHomePage() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <p>
+        Choose your color
+      </p>
+      <ul>
+        <li>
+          <Link href="/exercises/01-screensaver/lavender">
+            lavender
+          </Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/peachpuff">
+            peachpuff
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverHomePage;
